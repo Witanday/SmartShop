e8b0b29420f7334d327918846d20ebb1f796957d
Witanday <witanday.cd@gmail.com>
2019-07-22 11:30:49 -0400

    completed signin and token

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,10 @@
 const User= require("../models/user");
 const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken"); // to generate signed token
+const expressJwt = require("express-jwt"); // for authorization check
+
+
+
 const {errorHandler}= require("../helpers/dbErrorHandler")
 
 
@@ -30,3 +35,44 @@ exports.signup= async(req,res)=>{
    
   
 }
+
+exports.signin= async (req,res)=>{
+    //find the user based on email
+    const {email,password}= req.body
+    try{
+        User.findOne({email},(err,user)=>{
+            if(err || !user){
+                return res.status(400).json({
+                    err:"User with that email does not exist. Please signup"
+                });
+            
+
+            }
+            //if user is found make sure the email and password match
+            // create authenticate method in user model
+           // console.log(user.password)
+            const match= bcrypt.compareSync(password,user.password)
+            if(match){
+
+          
+            //generate a signed token with user id and secret
+            const secret = process.env.JWT_SECRET;
+            const token = jwt.sign({_id:user._id}, secret);
+
+            //persist the token as 't' in cookie with expiry date
+
+            res.cookie('t', token, {expire:new Date() + 2000})
+
+            // return response with user and token to frontend client
+            const {_id,name,email,role}= user
+            return res.json({token,user:{_id,name,email,role}})
+            }
+            return res.status(401).json({error:"Invalid Password"})
+        })
+    }catch(err){
+        console.log(err)
+    }
+
+    
+}
+
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {signup} = require("../controllers/user.js")
+const {signup, signin} = require("../controllers/user.js")
 const {validate}= require("../validator")
 
 router.post("/signup",validate('signup') ,signup);
 
-router.post("/signin",validate('signup') ,signin);
+router.post("/signin",signin);
 
 
 module.exports = router;
